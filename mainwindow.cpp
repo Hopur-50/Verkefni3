eed5b93940c3de7a068e53b49ab1de99eb08a8c3
fanney83 <fanneyv12@ru.is>
2015-12-11 11:08:49 +0000

    tappar

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,71 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "repositories/computerrepository.h"
+#include "repositories/scientistrepository.h"
+#include <string>
+#include <QDebug>
+#include <QMainWindow>
+
+using namespace std;
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_GetAllScientists_clicked()
+{
+    //qDebug() << ui-> ScientistsView ->text();
+}
+
+void MainWindow::on_GetAllComputers_clicked()
+{
+    //qDebug() << ui->computerRepo.getAllComputers(currentOrder)->text();
+}
+
+void MainWindow::on_AddNewScientist_clicked()
+{
+
+}
+
+void MainWindow::on_AddNewComputer_clicked()
+{
+
+}
+
+void MainWindow::on_editScientist_clicked()
+{
+
+}
+
+void MainWindow::on_editComputer_clicked()
+{
+
+}
+
+void MainWindow::on_deleteScientist_clicked()
+{
+
+}
+
+void MainWindow::on_deleteComputer_clicked()
+{
+
+}
+
+void MainWindow::on_addRelations_clicked()
+{
+
+}
+
+
+
--- mainwindow.h
+++ mainwindow.h
@@ -13,10 +13,32 @@ class MainWindow : public QMainWindow
 
 public:
     explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
+     ~MainWindow();
+
+private slots:
+
+    void on_AddNewScientist_clicked();
+
+    void on_AddNewComputer_clicked();
+
+    void on_editScientist_clicked();
+
+    void on_editComputer_clicked();
+
+    void on_deleteScientist_clicked();
+
+    void on_deleteComputer_clicked();
+
+    void on_addRelations_clicked();
+
+    void on_GetAllScientists_clicked();
+
+    void on_GetAllComputers_clicked();
 
 private:
     Ui::MainWindow *ui;
+    std::string input_text;
+
 };
 
 #endif // MAINWINDOW_H
