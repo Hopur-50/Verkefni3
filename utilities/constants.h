1efef34c67a002116bbe93d29ec2ad2a8c32c6a7
Darri Valgarðsson <DarrMac@clients-pool-1.nat.ru.is>
2015-12-15 17:02:59 +0000

    confirm f rem rel

--- utilities/constants.h
+++ utilities/constants.h
@@ -16,6 +16,7 @@ namespace constants
     const std::string SELECT_ALL_COMPUTERS = "SELECT name, computerType, constructed, buildYear, id FROM Computers c";
     const QString CONFIRM_DEL_COMPUTER = "Are you sure you wish to delete the selected computer?";
     const QString CONFIRM_DEL_SCIENTIST = "Are you sure you wish to delete the selected scientist?";
+    const QString CONFIRM_DEL_RELATION = "Are you sure you wish to delete the selected relation?";
 }
 
 #endif // CONSTANTS_H
